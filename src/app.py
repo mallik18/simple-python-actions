5cc499affa706d6f1af743a8acfa0c643902ad77
mallik18 <mallikarjunkolar18@gmail.com>
2021-05-05 13:41:43 +0530

    stored config key in different file

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+import config
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = config.SECRET_KEY
 
 posts = [
     {
